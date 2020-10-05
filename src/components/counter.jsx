77d71cc9bdf4683e963bb5d81faaa520b4bb4b38
Rachana Devi <rachanadevi18@gmail.com>
2020-10-05 07:47:10 +0530

    [Rachana] Add. classes to show Rendering of classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,16 @@ import React, { Component } from "react";
 //you can also write it here export default
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
   };
 
   render() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    
     return (
       <React.Fragment>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+        <span style={{ fontSize: 30 }} className={classes}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
