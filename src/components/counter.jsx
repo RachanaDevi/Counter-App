2a14ad45c138c37c3af32b69880fc8122a1cb8dd
Rachana Devi <rachanadevi18@gmail.com>
2020-10-05 23:19:45 +0530

    [Rachana] Refactor. use only handleIncrement function to have args

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,10 +12,6 @@ class Counter extends Component {
     this.setState({ count: this.state.count + 1 });
   };
 
-  doHandleIncrement = () => {
-    this.handleIncrement({ id: 1 });
-  };
-
   render() {
     return (
       <React.Fragment>
@@ -23,7 +19,7 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={this.doHandleIncrement}
+          onClick={() => this.handleIncrement({ id: this.state.count })}
           className="btn btn-secondary btn-sm"
         >
           Increment
