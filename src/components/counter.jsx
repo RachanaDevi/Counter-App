b35b805ff792bcc23ca48f3e9115647ecc7468a5
Rachana Devi <rachanadevi18@gmail.com>
2020-10-05 08:39:30 +0530

    [Rachana] Handle onClick event by passing ref of handleIncrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,13 +7,21 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
+  handleIncrement() {
+    console.log("Increment clicked!");
+  }
   render() {
     return (
       <React.Fragment>
         <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
