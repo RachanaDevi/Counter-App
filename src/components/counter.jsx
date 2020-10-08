7e23bb907b317dcfe0b562d784c29ffc9546ad92
Rachana Devi <rachanadevi18@gmail.com>
2020-10-08 07:43:31 +0530

    [Rachana] Add. props in Counter set in Counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,41 +1,41 @@
 import React, { Component } from "react";
 
-//you can also write it here export default
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: [],
   };
 
   handleIncrement = (product) => {
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log("PROPS", this.props);
     return (
       <div>
         <span style={{ fontSize: 30 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement({ id: this.state.count })}
+          onClick={() => this.handleIncrement({ id: this.state.value })}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
       </div>
-    ); //jsx expression which calls React.createElement, so you have to import
+    );
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 export default Counter;
